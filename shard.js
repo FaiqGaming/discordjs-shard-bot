a0348ec15c54dcb0ad7f42e9323462f82254bc61
SiapaTauKamiIniAdalahwgkwoe[hkwepgk23g;24 <39037579+FaiqGaming@users.noreply.github.com>
2018-11-01 11:46:32 +0900

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,13 +1,9 @@
-const config = require('./config.json')
-
 const { ShardingManager } = require('discord.js');
 const shard = new ShardingManager('./app.js', {
-  token: config.token,
+  token: process.env.BOT_TOKEN,
   autoSpawn: true
 });
 
 shard.spawn(2);
 
 shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/${shard.totalShards}`));
-
-
